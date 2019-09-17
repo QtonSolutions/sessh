56f91faf4f38e1909ee138cb1a52d9dc3b0d8d6c
Stuart Chapman <StuBez@users.noreply.github.com>
2019-09-17 09:12:44 +0100

    pyinstaller command fixes

--- scripts/release.py
+++ scripts/release.py
@@ -16,13 +16,13 @@ def running_on_linux():
 
 
 if __name__ == '__main__':
-    add_data_contents = 'config.default.py;.'
+    add_data_contents = 'config.default.py:.'
     if running_on_windows():
         add_data_contents = 'config.default.py;.'
 
     sys.exit(
         subprocess.run(
             ['pyinstaller', 'main.py', '--add-data', add_data_contents, '--hidden-import=configparser',
-             '--noconfirm', '--onefile', '--name sessh']
+             '--noconfirm', '--onefile', '--name', 'sessh']
         ).returncode
     )
