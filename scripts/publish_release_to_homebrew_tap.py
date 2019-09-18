2353b5ab7665af91a5476296991382bc099fc0d1
Stuart Chapman <StuBez@users.noreply.github.com>
2019-09-18 12:25:12 +0100

    Paths

--- scripts/publish_release_to_homebrew_tap.py
+++ scripts/publish_release_to_homebrew_tap.py
@@ -54,7 +54,7 @@ def commit_and_push(version):
 def set_up_github_credentials():
     github_auth_token = os.environ["GH_TOKEN"]
     subprocess.run(['git', 'config', 'credential.helper', 'store --file=.git/credentials'], cwd=HOMEBREW_TAP_PATH)
-    with open('.git/credentials', 'w') as credentials:
+    with open(os.path.join(HOMEBREW_TAP_PATH, '.git/credentials'), 'w') as credentials:
         credentials.write(f'https://${github_auth_token}:@github.com')
     subprocess.run(['git', 'config', '--local', 'user.name', 'Travis CI'], cwd=HOMEBREW_TAP_PATH)
     subprocess.run(['git', 'config', '--local', 'user.email', 'travis@travis-ci.org'], cwd=HOMEBREW_TAP_PATH)
