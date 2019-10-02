39607c2dd3347056f7c43f77253fa83b3d46ce6f
Stuart Chapman <StuBez@users.noreply.github.com>
2019-10-02 15:24:30 +0100

    Use `os.pathsep` for path separator
    This saves manually specifying the the path search separator depending
    on the operating system that PyInstaller is running on.

--- scripts/release.py
+++ scripts/release.py
@@ -1,31 +1,13 @@
 import argparse
 import os
-import platform
 import subprocess
 import sys
-
-
-def running_on_macos():
-    return platform.system() == 'Darwin'
-
-
-def running_on_windows():
-    return platform.system() == 'Windows'
-
-
-def running_on_linux():
-    return platform.system() == 'Linux'
-
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Build a new release")
     parser.add_argument('version', help="version tag")
 
     args = parser.parse_args()
-
-    add_data_contents = 'config.default.py:.'
-    if running_on_windows():
-        add_data_contents = 'config.default.py;.'
 
     # The Python interpreter name and path differs depending on the environment
     interpreter_path = sys.executable
@@ -35,7 +17,7 @@ if __name__ == '__main__':
 
     sys.exit(
         subprocess.run(
-            ['pyinstaller', 'main.py', '--add-data', add_data_contents, '--hidden-import=configparser',
+            ['pyinstaller', 'main.py', '--add-data', f'config.default.py{os.pathsep}.', '--hidden-import=configparser',
              '--noconfirm', '--onefile', '--name', 'sessh'], check=True
         ).returncode
     )
